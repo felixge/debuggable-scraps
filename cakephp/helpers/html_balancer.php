8c5eb47bf7be11ecc3cfa57a4208946d22a4c376
Tim Koschuetzki <tim@debuggable.com>
2008-12-14 09:22:20 +0100

    adding html balancer helper

---
+++ cakephp/helpers/html_balancer.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * Created: Sat February 26 EDT 2008
+ * 
+ * A class to balance out html tags in texts. Makes your texts ready for excerpts with the TextHelper's truncate function.
+ * 
+ *
+ * Copyright (c) Debuggable Ltd. <http://debuggable.com>
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright		Copyright (c) 2008, Debuggable Ltd. <http://debuggable.com>
+ * @link			http://www.debuggable.com
+ * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+class HtmlBalancerHelper extends Helper {
+/**
+ * An array of html tags that are singled ended: <img />
+ *
+ * @var string
+ */
+	var $singleTags = array('br', 'hr', 'img', 'input');
+/**
+ * An array of nestable html tags
+ *
+ * @var string
+ */
+	var $nestableTags = array('blockquote', 'div', 'span'); 
+/**
+ * Keeps track of which tags are in the text and thus need to be ended
+ *
+ * @var string
+ */
+	var $tagStack = array();
+/**
+ * String of closing tags that is appended to the text to close all unclosed open tags
+ *
+ * @var string
+ */
+	var $tagQueue = '';
+/**
+ * Resets stack and queue for a new processing
+ *
+ * @return void
+ */
+	function reset() {
+		$this->tagStack = array();
+		$this->tagQueue = '';
+	}
+/**
+ * Main function of this helper: clean and balance out html tags for a given string
+ *
+ * @param string $txt 
+ * @return void
+ */
+	function process($txt) {
+		$this->reset();
+		$result = '';
+
+		// just to cache the stack size and to prevent countless count()s
+		$stackSize = 0;
+
+		$txt = $this->takeOutComments($txt);
+		$txt = $this->clearUnclosedTagsAtEnd($txt);
+
+		while (preg_match("#<(/?\w*)\s*([^>]*)>#", $txt, $matches)) {
+			$result .= $this->tagQueue;
+			$this->tagQueue = '';
+
+			$tagPos = strpos($txt, $matches[0]);
+			$tagLength = strlen($matches[0]);
+			$isStartingTag = $matches[1][0] != "/";
+
+			if ($isStartingTag) {
+				$tag = low($matches[1]);
+
+				$isSingleTag = in_array($tag, $this->singleTags);
+				$tagNotEmpty = (substr($matches[2], -1) != '/' && $tag != '');
+
+				if ($tagNotEmpty && $isSingleTag) {
+					$matches[2] .= '/';
+				} else {
+					$isNoNestableTag = !in_array($tag, $this->nestableTags);
+					$isLastTag = ($stackSize > 0 && $this->tagStack[$stackSize - 1] == $tag);
+
+					if ($isNoNestableTag && $isLastTag) {
+						$this->tagQueue = '</'.array_pop($this->tagStack).'>';
+					}
+					$stackSize = array_push($this->tagStack, $tag);
+				}
+
+				$attributes = $matches[2];
+				if ($attributes) {
+					$attributes = ' ' . $attributes;
+				}
+				$tag = '<' . $tag . $attributes . '>';
+
+				if (!empty($this->tagQueue)) {
+					$this->tagQueue .= $tag;
+					$tag = '';
+				}
+			} else {
+				$tag = low(substr($matches[1], 1));
+
+				if ($stackSize <= 0) {
+					$tag = '';
+				} elseif ($this->tagStack[$stackSize - 1] == $tag) {
+					$tag = '</' . $tag . '>';
+					array_pop($this->tagStack);
+					$stackSize--;
+				} else {
+					for ($j = $stackSize - 1; $j >= 0; $j--) {
+						if ($this->tagStack[$j] == $tag) {
+							for ($k = $stackSize - 1; $k >= $j; $k--) {
+								$this->tagQueue .= '</' . array_pop($this->tagStack) . '>';
+								$stackSize--;
+							}
+							break;
+						}
+					}
+					$tag = '';
+				}
+			}
+			$result .= substr($txt, 0, $tagPos).$tag;
+			$txt = substr($txt, $tagPos + $tagLength);
+		}
+		$result .= $this->tagQueue . $txt;
+
+		$result = $this->addRemainingTags($result);
+		$result = $this->reAddComments($result);
+
+		return $result;
+	}
+/**
+ * Closes all tags in the text that are in the tag stack
+ *
+ * @param string $txt 
+ * @return void
+ */
+	function addRemainingTags($txt) {
+		while ($x = array_pop($this->tagStack)) {
+			$txt .= '</' . $x . '>';
+		}
+		return $txt;
+	}
+/**
+ * Clean up shitty comments ;p No idea for a better name for this...
+ * It basically replaces comments temporarily with something else, so comments are not balanced out.
+ * Haha how would that work anyway?
+ *
+ * @param string $txt
+ * @return void
+ */
+	function takeOutComments($txt) {
+		$txt = r('< !--', '<    !--', $txt);
+		$txt = preg_replace('#<([0-9]{1})#', '&lt;$1', $txt);
+		return $txt;
+	}
+/**
+ * Re-replace the comments. : )
+ *
+ * @param string $txt
+ * @return void
+ */
+	function reAddComments($txt) {
+		$txt = r('< !--', '<!--', $txt);
+		$txt = r('<    !--', '< !--', $txt);
+		return $txt;
+	}
+/**
+ * Well cleans the '<div ' in <span>lala</span><div '. This is useful when you use CakePHP's truncate function
+ * (from TextHelper) to build text excerpts, but use html in these texts. ; / Truncate could cut a html tag in two. ; /
+ * 
+ * @param string $txt
+ * @return void
+ */
+	function clearUnclosedTagsAtEnd($txt) {
+		$tags = array('a', 'form', 'input', 'textarea', 'select', 'option', 'optiongroup', 'img', 
+			'table', 'th', 'tr', 'td', 'div', 'span', 'p', 'label', 'fieldset', 'legend', 
+			'style', 'ul', 'ol', 'li'
+		);
+
+		foreach ($tags as $tag) {
+			$length = strlen($tag);
+
+			for ($i = 0; $i < $length - 1; $i++) {
+				$current = substr($tag, 0, $i);
+				$pattern = '/^(.*?)<'.$current.'\s*([^>]*)$/im';
+				if (preg_match($pattern, $txt)) {
+					$txt = preg_replace($pattern, '$1', $txt);
+				}
+			}
+		}
+
+		return $txt;
+	}
+}
+?>
---
+++ cakephp/helpers/html_balancer.test.php
@@ -0,0 +1,198 @@
+<?php
+App::Import('Helper', 'HtmlBalancer');
+class HtmlCleanerTestCase extends UnitTestCase {
+	function setUp() {
+		$this->testObject = new HtmlBalancerHelper;
+	}
+	
+	function testUnembeddableTags() {
+		$input = '<p>some text here';
+		$expected = '<p>some text here</p>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<p>some <p>text here';
+		$expected = '<p>some </p><p>text here</p>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<p>some text here</p><p>';
+		$expected = '<p>some text here</p><p></p>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+
+		$input = '<a>some text here';
+		$expected = '<a>some text here</a>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<a>some <a>text here';
+		$expected = '<a>some </a><a>text here</a>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<a>some text here</a><a>';
+		$expected = '<a>some text here</a><a></a>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+	}
+
+	function testSingleTags() {
+		$input = '<img>some text here';
+		$expected = '<img />some text here';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = 'some text here<br>';
+		$expected = 'some text here<br />';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<br>some <br> <br> text <br> here<br><br>';
+		$expected = '<br />some <br /> <br /> text <br /> here<br /><br />';
+		$this->assertEqual($expected, $this->testObject->process($input));
+
+		$input = '<br >some <br > <br > text <br > here<br ><br >';
+		$expected = '<br />some <br /> <br /> text <br /> here<br /><br />';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<hr> <hr >some <hr > <hr > text <hr > here<hr ><hr >';
+		$expected = '<hr /> <hr />some <hr /> <hr /> text <hr /> here<hr /><hr />';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<input >some <input > <input > text <input > here<input ><input >';
+		$expected = '<input />some <input /> <input /> text <input /> here<input /><input />';
+		$this->assertEqual($expected, $this->testObject->process($input));
+	}
+	
+	function testEmbeddableTags() {
+		$input = '<div>some text here';
+		$expected = '<div>some text here</div>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<div>some <div>text here';
+		$expected = '<div>some <div>text here</div></div>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<div>some text here</div><div>';
+		$expected = '<div>some text here</div><div></div>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+
+
+		$input = '<span>some text here';
+		$expected = '<span>some text here</span>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<span>some <span>text here';
+		$expected = '<span>some <span>text here</span></span>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<span>some text here</span><span>';
+		$expected = '<span>some text here</span><span></span>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+
+
+		$input = '<blockquote>some text here';
+		$expected = '<blockquote>some text here</blockquote>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<blockquote>some <blockquote>text here';
+		$expected = '<blockquote>some <blockquote>text here</blockquote></blockquote>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<blockquote>some text here</blockquote><blockquote>';
+		$expected = '<blockquote>some text here</blockquote><blockquote></blockquote>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		
+		$input = '<blockquote><span>some text here';
+		$expected = '<blockquote><span>some text here</span></blockquote>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<blockquote>some <blockquote>text here<span>';
+		$expected = '<blockquote>some <blockquote>text here<span></span></blockquote></blockquote>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<blockquote>some text here</span><blockquote>';
+		$expected = '<blockquote>some text here<blockquote></blockquote></blockquote>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<blockquote>some <span>text</span2> here</span><blockquote>';
+		$expected = '<blockquote>some <span>text here</span><blockquote></blockquote></blockquote>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+	}
+	
+	function testAttributes() {
+		$input = '<img src="testme">some text here';
+		$expected = '<img src="testme"/>some text here';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = 'some text <div style="lala">here';
+		$expected = 'some text <div style="lala">here</div>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = '<div cleanup="here" isFun="yes">';
+		$expected = '<div cleanup="here" isFun="yes"></div>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+	}
+	
+	function testUnclosedTagsAtEnd() {
+		$input = 'some text here<div';
+		$expected = 'some text here';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = 'some text here<div   ';
+		$expected = 'some text here';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = 'some text here <span <span  ';
+		$expected = 'some text here ';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = "some text here \n<span ";
+		$expected = "some text here \n";
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = "some text here <div  \n></div><span ";
+		$expected = "some text here \n>";
+		$this->assertEqual($expected, $this->testObject->process($input));
+	}
+	
+	function testReallyInvalidHtml() {
+		$input = 'some </span><div>text<p> </div';
+		$expected = 'some <div>text<p> </p></div>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = 'some </blockquote>text <div>here<div   <blockquote';
+		$expected = 'some text <div>here</div>';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = 'some text here <d  ';
+		$expected = 'some text here ';
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = 'some text here <spa';
+		$expected = 'some text here ';
+		$this->assertEqual($expected, $this->testObject->process($input));
+
+	}
+	
+	function testComplex() {
+		$input = <<<HTML
+<p>For your convenience, I've also uploaded my slides to <a href="http://www.slideshare.net/the_undefined">my slideshare account</a>.</p><div
+HTML;
+		$expected = <<<HTML
+<p>For your convenience, I've also uploaded my slides to <a href="http://www.slideshare.net/the_undefined">my slideshare account</a>.</p>
+HTML;
+		$this->assertEqual($expected, $this->testObject->process($input));
+		
+		$input = <<<HTML
+<div class="body"><p>For your convenience, I've also uploaded my slides to <a href="http://www.slideshare.net/the_undefined">my slideshare account</a>.</p><div
+HTML;
+		$expected = <<<HTML
+<div class="body"><p>For your convenience, I've also uploaded my slides to <a href="http://www.slideshare.net/the_undefined">my slideshare account</a>.</p></div>
+HTML;
+		$this->assertEqual($expected, $this->testObject->process($input));
+	
+	$input = <<<HTML
+	<div class="body"><p>For your convenience, I've also uploaded my slides to <a href="http://www.slideshare.net/the_undefined">my slideshare account</a>.</p><div style="something here"
+HTML;
+			$expected = <<<HTML
+	<div class="body"><p>For your convenience, I've also uploaded my slides to <a href="http://www.slideshare.net/the_undefined">my slideshare account</a>.</p></div>
+HTML;
+			$this->assertEqual($expected, $this->testObject->process($input));
+	}
+}
+?>
